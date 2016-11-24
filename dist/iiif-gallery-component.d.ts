ccdf708c60819d9568407f4761eaeeedcf9a6be2
edsilv <e.silverton@gmail.com>
2016-11-24 14:41:10 +0000

    updated base-component and gulp-tasks

--- dist/iiif-gallery-component.d.ts
+++ dist/iiif-gallery-component.d.ts
@@ -1,4 +1,24 @@
 // iiif-gallery-component v1.0.2 https://github.com/viewdir/iiif-gallery-component#readme
+// component-specific definitions
+
+interface JQuery {
+    // jsviews
+    link: any;
+    render: any;
+    // unevent
+    on(events: string, handler: (eventObject: JQueryEventObject, ...args: any[]) => any, wait: Number): JQuery;
+    // plugins
+    checkboxButton(onClicked: (checked: boolean) => void);
+    swapClass(removeClass: string, addClass: string): void;
+}
+
+interface JQueryStatic {
+    // jsviews
+    observable: any;
+    templates: any;
+    views: any;
+    view: any;
+}
 declare namespace IIIFComponents {
     class GalleryComponent extends _Components.BaseComponent implements IGalleryComponent {
         options: IGalleryComponentOptions;
