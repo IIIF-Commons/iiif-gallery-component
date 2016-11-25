3899471532cb397692c0b17b585fe8355c43de3f
edsilv <e.silverton@gmail.com>
2016-11-25 18:41:47 +0000

    added jsviews.d.ts

---
+++ typings/jsviews.d.ts
@@ -0,0 +1,18 @@
+interface JQuery {
+    // jsviews
+    link: any;
+    render: any;
+    // unevent
+    on(events: string, handler: (eventObject: JQueryEventObject, ...args: any[]) => any, wait: Number): JQuery;
+    // plugins
+    checkboxButton(onClicked: (checked: boolean) => void);
+    swapClass(removeClass: string, addClass: string): void;
+}
+
+interface JQueryStatic {
+    // jsviews
+    observable: any;
+    templates: any;
+    views: any;
+    view: any;
+}
