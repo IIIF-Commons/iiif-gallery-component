a1935026a57325b7115df7228624647cb45f57af
edsilv <e.silverton@gmail.com>
2017-12-05 22:08:13 +0000

    updated manifesto

--- dist/iiif-gallery-component.d.ts
+++ dist/iiif-gallery-component.d.ts
@@ -1,4 +1,4 @@
-// iiif-gallery-component v1.1.1 https://github.com/iiif-commons/iiif-gallery-component#readme
+// iiif-gallery-component v1.1.2 https://github.com/iiif-commons/iiif-gallery-component#readme
 /// <reference types="jquery" />
 interface JQuery {
     link: any;
